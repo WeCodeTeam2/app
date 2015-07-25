45c73487d27ae3abe715b689e12c6d843798f8ae
Katie Musick <kmusick314@gmail.com>
2015-07-25 13:26:53 -0700

    updated app.js to maybe init efb

--- public/js/app.js
+++ public/js/app.js
@@ -9,8 +9,32 @@ diversityApp.config(function (ezfbProvider) {
 
 var diversityAppControllers = angular.module('diversityAppControllers', []);
 
-diversityAppControllers.controller('DivAppController', ['$scope', '$routeParams', function ($scope, $routeParams) {
+diversityAppControllers.controller('DivAppController', ['$scope', '$routeParams', 'ezfb', function ($scope, $routeParams, ezfb) {
   $scope.company = $routeParams.company;
+
+  updateLoginStatus(updateApiMe);
+
+  $scope.login = function () {
+	  ezfb.login(function (res) {
+		  if (res.authResponse) {
+			  updateLoginStatus(updateApiMe);
+		  }
+	  }, {scope: 'email,user_likes'});
+  };
+
+  function updateLoginStatus (more) {
+	  ezfb.getLoginStatus(function (res) {
+		  $scope.loginStatus = res;
+
+		  (more || angular.noop)();
+	  });
+  }
+
+  function updateApiMe () {
+	  ezfb.api('/me', function (res) {
+		  $scope.apiMe = res;
+	  });
+  }
 }]);
 
 diversityAppControllers.config(['$routeProvider',
